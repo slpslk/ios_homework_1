aca161acc83f965d817e79c0046238f502be0e64
Sofya Avtsinova <sofiavchinova@gmail.com>
2024-09-24 22:24:09 +0700

    first commit

---
+++ Homework_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework_1
+//
+//  Created by Sofya Avtsinova on 24.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
